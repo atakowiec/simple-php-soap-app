b57434cc2b88f51690da126ca6c9ec9fb6aff9d1
atakowiec <mat.cieszczyk@gmail.com>
2024-07-29 11:17:10 +0200

    minor docker config fix

--- soap-service/src/soap_service.php
+++ soap-service/src/soap_service.php
@@ -4,7 +4,7 @@ ini_set("soap.wsdl_cache_enabled", "0");
 class SoapService {
     public function getWeatherData($city) {
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "https://api.openweathermap.org/data/2.5/weather?q=$city&appid={$_ENV['weather-api-key']}&units=metric");
+        curl_setopt($curl, CURLOPT_URL, "https://api.openweathermap.org/data/2.5/weather?q=$city&appid={$_ENV['weather_api_key']}&units=metric");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
         $result = curl_exec($curl);
